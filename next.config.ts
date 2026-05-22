ac1156a6e2824e5f8d4d99e9a1c26f72ebf19abb
John Mark Anunciado <johnmark.anunciado@clearmindai.com>
2026-05-22 23:19:39 +0800

    latest commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // 👈 add this
+
   images: {
     remotePatterns: [
-      { protocol: 'http', hostname: 'localhost', port: '8000', pathname: '/storage/**' },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/storage/**",
+      },
     ],
-    unoptimized: true,
+    unoptimized: true, // required for static export
   },
 };
 
